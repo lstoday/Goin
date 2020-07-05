d17d1b9a61523634395aaad410769c897d25fbb3
yali <yali_today@163.com>
2020-07-05 15:08:19 +0800

    Initial Commit

---
+++ GoIn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoIn
+//
+//  Created by yali on 2020/7/5.
+//  Copyright © 2020 yali. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
