4d9819f85c226c79b69df70ec068edd2bca47647
yali <yali_today@163.com>
2021-01-05 03:53:21 +0800

    commit files

--- GoIn/ContentView.swift
+++ GoIn/ContentView.swift
@@ -8,9 +8,29 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var time = ""
+    @State private var name = ""
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            ZStack {
+                Image("backImg").resizable().onAppear(perform: getTime).scaledToFill().offset(x: 0, y: -10)
+//                TextField("",text : $time).offset(x: 94, y: 118)
+//                TextField("",text : $name).offset(x: 88, y: 53)
+                Text(name).font(.system(size: 15, weight: .light, design: .serif)).offset(x: -104, y: 53)
+                Text(time).font(.system(size: 17)).offset(x: -30, y: 118)
+                
+            }
+        }
+    }
+    func getTime() {
+        let date = Date()
+        let dateFormater = DateFormatter()
+        
+        dateFormater.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        time  = dateFormater.string(from: date)
+        name = "李松"
     }
 }
 
--- GoIn/ViewController.swift
+++ GoIn/ViewController.swift
@@ -1,9 +1,31 @@
-//
-//  UIViewController.swift
-//  GoIn
-//
-//  Created by yali on 2020/7/5.
-//  Copyright © 2020 yali. All rights reserved.
-//
 
-import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        scondDemo()
+    }
+
+    func scondDemo() {
+        let myView = UILabel(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        let time = getTime()
+        myView.text = time
+        //添加到根视图
+        view.addSubview(myView)
+        
+        
+    }
+    
+    func getTime() -> String {
+        let date = Date()
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateFormat = "yyy-MM-dd HH:mm:ss"
+        return timeFormatter.string(from: date)
+    }
+    
+
+}
